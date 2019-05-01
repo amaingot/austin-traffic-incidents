2f8d1675ac1346d7054087dc4b1dc7b25c5834d5
Alex Maingot <alex.maingot@gmail.com>
2019-05-01 16:57:59 -0500

    clean it up

--- amplify/backend/function/crashFunction/ts-src/storeCrashData.ts
+++
@@ -1,8 +0,0 @@
-import * as AWS from 'aws-sdk';
-import { TrafficIncidentList } from './queryApi';
-
-const storeCrashData = async (data: TrafficIncidentList): Promise<void> => {
-  
-};
-
-export default storeCrashData;
