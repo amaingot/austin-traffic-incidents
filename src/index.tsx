d02fd380078abdc8ac07d6fb9f3b80d6e7414850
Alex Maingot <alex.maingot@gmail.com>
2019-05-02 13:30:10 -0500

    Adding amplify

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,32 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import Amplify, { Auth } from 'aws-amplify';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import { Rehydrated } from 'aws-appsync-react';
+import { ApolloProvider } from 'react-apollo';
+
+import awsconfig from 'src/aws-exports';
 import Main from './Main';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+Amplify.configure(awsconfig);
+
+const client = new AWSAppSyncClient({
+  url: awsconfig.aws_appsync_graphqlEndpoint,
+  region: awsconfig.aws_appsync_region,
+  auth: {
+    type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+    jwtToken: async () => (await Auth.currentSession()).getAccessToken().getJwtToken(),
+  },
+  // Uses Amazon IAM credentials to authorize requests to S3.
+  complexObjectsCredentials: async () => Auth.currentCredentials(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client as any}>
+    <Rehydrated>
+      <Main />
+    </Rehydrated>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
