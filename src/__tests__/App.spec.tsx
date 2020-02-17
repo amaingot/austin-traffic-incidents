a9f0d84648213eb30a1b3a0148f7a3446ff5c95a
Alex Maingot <alex.maingot@gmail.com>
2020-02-17 07:31:05 -0600

    Starting fresh

--- src/__tests__/App.spec.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { render } from 'react-testing-library';
-
-import Main from 'src/Main';
-
-test('Main should render', () => {
-  render(<Main />);
-});
--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare var App: {
-  env: string;
-  path: string;
-  scriptPath: string;
-  onDev: boolean;
-  webEnv: string;
-};
-
-declare module 'aws-amplify-react';
